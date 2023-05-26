d72ee824b94fb79ef06fec2db06c720327fc2eda
lamerest <vladykin.190103@gmail.com>
2023-05-26 23:30:26 +0300

    Updated swap logic to UniswapV3 version

--- contracts/SwapProxy.sol
+++ contracts/SwapProxy.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@uniswap/universal-router/contracts/libraries/Commands.sol";
+import "@uniswap/universal-router/contracts/modules/uniswap/v3/BytesLib.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./UniswapRouter.sol";
+import "@uniswap/universal-router/contracts/interfaces/IUniversalRouter.sol";
 import "./libraries/SafeERC20.sol";
 
 contract SwapProxy is Ownable {
 	using SafeERC20 for IERC20;
-	IUniswapV2Router01 private _router;
+
+	IUniversalRouter public router;
+	address WETH;
+
 	address private _feeReceiver;
 	uint256 private _feeInBasisPoints;
 
@@ -17,129 +22,167 @@ contract SwapProxy is Ownable {
 
 	event FeeReceiverUpdated(address oldFeeReceiver, address newFeeReceiver);
 
-	event SwapWithFee(
-		uint256 amountIn,
-		uint256 amountOut,
-		uint256 amountWithoutFee,
-		uint256 amountOutWithoutFee,
-		uint256 fee
-	);
+	// event SwapWithFee(
+	// 	uint256 amountIn,
+	// 	uint256 amountOut,
+	// 	uint256 amountWithoutFee,
+	// 	uint256 amountOutWithoutFee,
+	// 	uint256 fee
+	// );
 
 	constructor(
 		address router_,
+		address weth_,
 		address feeReceiver_,
 		uint256 feeInBasisPoints_
 	) {
-		_router = IUniswapV2Router01(router_);
+		router = IUniversalRouter(router_);
+		WETH = weth_;
+
 		_feeReceiver = feeReceiver_;
 		_feeInBasisPoints = feeInBasisPoints_;
 	}
 
-	function swapExactTokensForTokens(
-		uint256 amountIn,
-		uint256 amountOutMin,
-		address[] calldata path,
-		address to,
+	function executePlain(
+		address inputToken,
+		uint256 amount,
+		bytes calldata commands,
+		bytes[] memory inputs,
 		uint256 deadline
-	) external returns (uint256[] memory amounts) {
-		(uint256 newAmountIn, uint256 newAmountOut) = deductFee(path[0], amountIn, amountOutMin);
+	) public payable {
+		if (inputToken == WETH) {
+			// uint256 fee = deductEthFeeOptimized();
+			router.execute{value: msg.value}(commands, inputs, deadline);
+		} else {
+			IERC20 token = IERC20(inputToken);
 
-		IERC20(path[0]).approve(address(_router), newAmountIn);
+			token.safeTransferFrom(msg.sender, address(this), amount);
+			token.approve(address(router), amount);
 
-		amounts = _router.swapExactTokensForTokens(newAmountIn, newAmountOut, path, to, deadline);
+			router.execute(commands, inputs, deadline);
+		}
 	}
 
-	function swapTokensForExactTokens(
-		uint256 amountOut,
-		uint256 amountInMax,
-		address[] calldata path,
-		address to,
+	/// @notice Executes swap on Uniswap UniversalRouter
+	/// @dev swap params must already changed to be without fee amount
+	/// @dev optimized version with off-chain calculation
+	function execute(
+		address inputToken,
+		uint256 amount,
+		bytes calldata commands,
+		bytes[] memory inputs,
 		uint256 deadline
-	) external returns (uint256[] memory amounts) {
-		(uint256 newAmountIn, uint256 newAmountOut) = deductFee(path[0], amountInMax, amountOut);
-
-		IERC20(path[0]).approve(address(_router), newAmountIn);
-
-		amounts = _router.swapTokensForExactTokens(newAmountOut, newAmountIn, path, to, deadline);
+	) public payable {
+		if (inputToken == WETH) {
+			uint256 fee = deductEthFeeOptimized();
+			router.execute{value: msg.value - fee}(commands, inputs, deadline);
+		} else {
+			deductFeeOptimized(inputToken, amount);
+			router.execute(commands, inputs, deadline);
+		}
 	}
 
-	function swapExactETHForTokens(
-		uint256 amountOutMin,
-		address[] calldata path,
-		address to,
-		uint256 deadline
-	) external payable returns (uint256[] memory amounts) {
-		(uint256 newAmountIn, uint256 newAmountOut) = deductEthFee(amountOutMin);
+	// Complex logic with high call cost
+	// function execute1(
+	// 	bytes calldata commands,
+	// 	bytes[] memory inputs,
+	// 	uint256 deadline
+	// ) public payable {
+	// 	uint256 numCommands = commands.length;
 
-		amounts = _router.swapExactETHForTokens{value: newAmountIn}(newAmountOut, path, to, deadline);
-		// amounts = _router.swapExactETHForTokens{value: msg.value}(amountOutMin, path, to, deadline);
+	// 	// loop through all given commands, execute them and pass along outputs as defined
+	// 	for (uint256 commandIndex = 0; commandIndex < numCommands; ) {
+	// 		bytes1 command = commands[commandIndex];
+
+	// 		bytes memory input = inputs[commandIndex];
+
+	// 		// Replace input with transformed one
+	// 		inputs[commandIndex] = deductFeeAndTransformInputs(command, input);
+
+	// 		unchecked {
+	// 			commandIndex++;
+	// 		}
+	// 	}
+
+	// 	router.execute(commands, inputs, deadline);
+	// }
+
+	// function deductFeeAndTransformInputs(bytes1 command, bytes memory input) public returns (bytes memory) {
+	// 	if (
+	// 		uint256(bytes32(command)) == Commands.V3_SWAP_EXACT_IN ||
+	// 		uint256(bytes32(command)) == Commands.V3_SWAP_EXACT_OUT
+	// 	) {
+	// 		(address recipient, uint256 amountIn, uint256 amountOut, bytes memory path, bool fundsFromMsgSender) = abi
+	// 			.decode(input, (address, uint256, uint256, bytes, bool));
+
+	// 		address inputToken = this.getFirstTokenFromPath(path);
+
+	// 		uint256 newAmountIn;
+	// 		uint256 newAmountOut;
+
+	// 		if (inputToken == WETH) {
+	// 			(newAmountIn, newAmountOut) = deductEthFee(amountOut);
+	// 		} else {
+	// 			(newAmountIn, newAmountOut) = deductFee(inputToken, amountIn, amountOut);
+	// 		}
+
+	// 		return abi.encode(recipient, newAmountIn, newAmountOut, path, fundsFromMsgSender);
+	// 	} else if (
+	// 		uint256(bytes32(command)) == Commands.WRAP_ETH || uint256(bytes32(command)) == Commands.UNWRAP_WETH
+	// 	) {
+	// 		(address recipient, uint256 amount) = abi.decode(input, (address, uint256));
+
+	// 		uint256 fee = (amount * _feeInBasisPoints) / 10000;
+
+	// 		uint256 newAmount = amount - fee;
+	// 		return abi.encode(recipient, newAmount);
+	// 	} else {
+	// 		return input;
+	// 	}
+	// }
+
+	// function deductFee(
+	// 	address srcToken,
+	// 	uint256 amountIn,
+	// 	uint256 amountOut
+	// ) internal returns (uint256 newAmountIn, uint256 newAmountOut) {
+	// 	uint256 fee = (amountIn * _feeInBasisPoints) / 10000;
+
+	// 	newAmountIn = amountIn - fee;
+	// 	newAmountOut = (amountOut * (10000 - _feeInBasisPoints)) / 10000;
+
+	// 	IERC20(srcToken).safeTransferFrom(msg.sender, address(this), amountIn);
+	// 	IERC20(srcToken).safeTransfer(_feeReceiver, fee);
+
+	// 	emit SwapWithFee(amountIn, amountOut, newAmountIn, newAmountOut, fee);
+	// }
+
+	// function deductEthFee(uint256 amountOut) internal returns (uint256 newAmountIn, uint256 newAmountOut) {
+	// 	uint256 fee = (msg.value * _feeInBasisPoints) / 10000;
+
+	// 	newAmountIn = msg.value - fee;
+	// 	newAmountOut = (amountOut * (10000 - _feeInBasisPoints)) / 10000;
+
+	// 	(bool success, ) = _feeReceiver.call{value: fee}("");
+	// 	require(success, "Transfer to receiver failed");
+
+	// 	emit SwapWithFee(msg.value, amountOut, newAmountIn, newAmountOut, fee);
+	// }
+
+	function deductFeeOptimized(address srcToken, uint256 amount) internal {
+		uint256 fee = (amount * _feeInBasisPoints) / 10000;
+		IERC20 token = IERC20(srcToken);
+
+		token.safeTransferFrom(msg.sender, address(this), amount);
+		token.safeTransfer(_feeReceiver, fee);
+		token.approve(address(router), amount - fee);
 	}
 
-	function swapTokensForExactETH(
-		uint256 amountOut,
-		uint256 amountInMax,
-		address[] calldata path,
-		address to,
-		uint256 deadline
-	) external returns (uint256[] memory amounts) {
-		(uint256 newAmountIn, uint256 newAmountOut) = deductFee(path[0], amountInMax, amountOut);
-
-		IERC20(path[0]).approve(address(_router), newAmountIn);
-
-		amounts = _router.swapTokensForExactETH(newAmountOut, newAmountIn, path, to, deadline);
-	}
-
-	function swapExactTokensForETH(
-		uint256 amountIn,
-		uint256 amountOutMin,
-		address[] calldata path,
-		address to,
-		uint256 deadline
-	) external returns (uint256[] memory amounts) {
-		(uint256 newAmountIn, uint256 newAmountOut) = deductFee(path[0], amountIn, amountOutMin);
-
-		IERC20(path[0]).approve(address(_router), newAmountIn);
-
-		amounts = _router.swapExactTokensForETH(newAmountIn, newAmountOut, path, to, deadline);
-	}
-
-	function swapETHForExactTokens(
-		uint256 amountOut,
-		address[] calldata path,
-		address to,
-		uint256 deadline
-	) external payable returns (uint256[] memory amounts) {
-		(uint256 newAmountIn, uint256 newAmountOut) = deductEthFee(amountOut);
-
-		amounts = _router.swapETHForExactTokens{value: newAmountIn}(newAmountOut, path, to, deadline);
-	}
-
-	function deductFee(
-		address srcToken,
-		uint256 amountIn,
-		uint256 amountOut
-	) internal returns (uint256 newAmountIn, uint256 newAmountOut) {
-		uint256 fee = (amountIn * _feeInBasisPoints) / 10000;
-
-		newAmountIn = amountIn - fee;
-		newAmountOut = (amountOut * (10000 - _feeInBasisPoints)) / 10000;
-
-		IERC20(srcToken).safeTransferFrom(msg.sender, address(this), amountIn);
-		IERC20(srcToken).safeTransfer(_feeReceiver, fee);
-
-		emit SwapWithFee(amountIn, amountOut, newAmountIn, newAmountOut, fee);
-	}
-
-	function deductEthFee(uint256 amountOut) internal returns (uint256 newAmountIn, uint256 newAmountOut) {
-		uint256 fee = (msg.value * _feeInBasisPoints) / 10000;
-
-		newAmountIn = msg.value - fee;
-		newAmountOut = (amountOut * (10000 - _feeInBasisPoints)) / 10000;
+	function deductEthFeeOptimized() internal returns (uint256 fee) {
+		fee = (msg.value * _feeInBasisPoints) / 10000;
 
 		(bool success, ) = _feeReceiver.call{value: fee}("");
 		require(success, "Transfer to receiver failed");
-
-		emit SwapWithFee(msg.value, amountOut, newAmountIn, newAmountOut, fee);
 	}
 
 	function updateFeePercent(uint256 feeInBasisPoints_) external onlyOwner {
@@ -154,5 +197,11 @@ contract SwapProxy is Ownable {
 		_feeReceiver = feeReceiver_;
 
 		emit FeeReceiverUpdated(oldFeeReceiver, _feeReceiver);
+	}
+
+	/// @dev Needed to call by this._bytesToAddress()
+	/// @dev otherwise toAddress  can't take bytes memory
+	function getFirstTokenFromPath(bytes calldata path) public pure returns (address) {
+		return BytesLib.toAddress(path);
 	}
 }
---
+++ scripts/checkSwapOnFork.ts
@@ -0,0 +1,44 @@
+import hardhat, { ethers } from 'hardhat';
+// eslint-disable-next-line camelcase
+import { SwapProxy__factory } from '../typechain-types';
+
+async function deploy() {
+	// hardhat.tracer.enabled = true;
+	const swapEthToUSDT = {
+		commands: '0x0b00',
+		value: '5000000000000000',
+		inputs: [
+			'0x00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000011c37937e08000',
+			// eslint-disable-next-line max-len
+			'0x00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000011c37937e0800000000000000000000000000000000000000000000000000000000000008b983a00000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002bc02aaa39b223fe8d0a0e5c4f27ead9083c756cc20001f4dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000000000000000000000',
+		],
+		deadline: Math.floor(new Date().getTime() / 1000),
+	};
+
+	const feeReceiver = '0x037ef1821002d716E3C612beb23DCF4Ef338A405';
+	const feeInBasisPoints = 100;
+	const universalRouter = '0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B';
+	const weth = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+	const deployArgs: [string, string, string, number] = [universalRouter, weth, feeReceiver, feeInBasisPoints];
+
+	// eslint-disable-next-line camelcase
+	const SwapProxy = (await ethers.getContractFactory('SwapProxy')) as SwapProxy__factory;
+	const swapProxy = await SwapProxy.deploy(...deployArgs);
+	await swapProxy.deployed();
+	// const address = '0x3797669a4616cdABd9F807a4E637DdB538C98345';
+
+	const tx = await swapProxy.executePlain(
+		weth,
+		swapEthToUSDT.value,
+		swapEthToUSDT.commands,
+		swapEthToUSDT.inputs,
+		swapEthToUSDT.deadline,
+	);
+}
+
+deploy()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.log(error);
+		process.exit(1);
+	});
