b2331dd2a6de72ccb54a299b699d3f40051e2a13
lamerest <vladykin.190103@gmail.com>
2023-05-30 16:19:15 +0300

    Installed and imported some helpful packages and contracts

---
+++ contracts/imports/imports.sol
@@ -0,0 +1,3 @@
+import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/IQuoterV2.sol";
+// import "@uniswap/v3-periphery/contracts/test/SwapRouter.sol";
